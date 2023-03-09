962255a56676c153fbb074dd1c9c8d2a8ef24dcd
Aman Singh <102784256+mov-aman@users.noreply.github.com>
2023-03-09 22:51:28 +0530

    1st Entry
    1st Java project entry.
    Book class: This class have the attributes of a book, such as title, author, ISBN, price, quantity, and any other relevant attributes.

---
+++ project.java
@@ -0,0 +1,17 @@
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private double price;
+    private int quantity;
+
+    public Book(String title, String author, String isbn, double price, int quantity) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // project is going on
+}
